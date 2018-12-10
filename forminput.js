ed0dc91c54bc6a80215e9bb3df608ed47cf6aeef
SAIKRISHNA <muddulurusaikrishna@gmail.com>
2018-12-10 01:53:57 -0800

    Temparature will be displayed in the console
    with '#' will be ignored, and an empty message aborts the commit.

--- forminput.js
+++ forminput.js
@@ -1,9 +1,3 @@
-// window.onload = function(){
-//     document.getElementById('send').onclick = function(){
-//         alert(document.getElementById("name").value);
-//         //return false;
-//     }
-//  }
 
 //Weather data
 
@@ -24,32 +18,11 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=7d4bd
     return result.json();
 })
 .then(data=>{
-    let output = '<h2>Weather Information</h2>';
-    data.forEach(function(weather){
-        output+=`
-        <div>
-            <h3>    Current Temperature in ${weather.name} is ${weather.main.temp}</h3>
-        </div>;`
-    });
-    document.getElementById('para').innerHTML=output;
-    // console.log(`Today's Temperatures in ${data.name} is ${data.main.temp} and the temparature 
-    // is between ${data.main.temp_min} - ${data.main.temp_max}` );
+    console.log(`Today's Temperatures in ${data.name} is ${data.main.temp} and the temparature 
+    is between ${data.main.temp_min} - ${data.main.temp_max}` );
 })
 .catch(error=> {
     console.log(error);
 });
 
 }
-
-
-
-
-// function weatherAsk(){
-//     console.log(city);
-//     var url = api + city + apikey + units;
-//     loadJSON(url,getData);
-// }
-
-// function getData(data){
-//     weather=data;
-// }
